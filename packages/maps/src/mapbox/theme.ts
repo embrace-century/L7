797d5451894f1ae19cb198e0797ca48e96a0bade
我天真无邪 <so.zengtao@gmail.com>
2020-12-18 22:29:24 +0800

    Update theme.ts

--- packages/maps/src/mapbox/theme.ts
+++ packages/maps/src/mapbox/theme.ts
@@ -4,6 +4,7 @@ export const MapTheme: {
   light: 'mapbox://styles/zcxduo/ck2ypyb1r3q9o1co1766dex29',
   dark: 'mapbox://styles/zcxduo/ck241p6413s0b1cpayzldv7x7',
   normal: 'mapbox://styles/mapbox/streets-v11',
+  hq: "mapbox://styles/mapbox/light-v10",
   blank: {
     version: 8,
     // sprite: 'https://lzxue.github.io/font-glyphs/sprite/sprite',
